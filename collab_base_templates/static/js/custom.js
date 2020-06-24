53b81c07936125eba44cf5532fd8cd36a6263495
Mattes Sarcander <info@fortschritt.pro>
2020-06-24 15:00:12 +0200

    Hav growls automatically disappear

--- collab_base_templates/static/js/custom.js
+++ collab_base_templates/static/js/custom.js
@@ -4,3 +4,16 @@
 $('.n12n h4 a').on('click', function(ev) {
     $(ev.target).blur()
 });
+
+/* auto-dismiss growls */
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("hi!");
+
+    function closeGrowls(){
+        $('#app-growl .alert').alert('close');
+    }
+    
+    setTimeout(closeGrowls, 2000);
+
+})
